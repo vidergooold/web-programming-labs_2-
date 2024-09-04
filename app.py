ef5d878089f80ebebf9efdd38a4f456f29dd1464
vidergold <iravidergold@yandex.ru>
2024-09-04 17:50:23 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <html>
              <body>
                   <h1>web-сервер на flask</h1>
              </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Видергольд Ирина Сергеевна"
+    group = "ФБИ-22"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
         </html>"""
