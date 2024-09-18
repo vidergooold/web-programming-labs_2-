f74de4e8bff94b57fc483fecd60fbef5001dd80e
vidergold <iravidergold@yandex.ru>
2024-09-18 20:02:41 +0700

    Имя студента

--- app.py
+++ app.py
@@ -486,6 +486,7 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Видергольд Ирина'
+    return render_template('example.html', name=name)
 
 
