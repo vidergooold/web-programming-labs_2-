42ea99421395d70f73fc796ac1f1583f1b17878f
vidergold <iravidergold@yandex.ru>
2024-12-21 10:58:19 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for
 from dotenv import load_dotenv
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -8,6 +12,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from rgz import rgz
 
 load_dotenv()
@@ -17,6 +22,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'sqlite') 
 
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'irina_vidergold_knowledge_base'
+    db_user = 'irina_vidergold_knowledge_base'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'irina_vidergold_knowledge_base.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app) 
+
 app.register_blueprint(lab1, url_prefix='/lab1')
 app.register_blueprint(lab2, url_prefix='/lab2')
 app.register_blueprint(lab3, url_prefix='/lab3', name='lab3')
@@ -24,6 +43,7 @@ app.register_blueprint(lab4, url_prefix='/lab4')
 app.register_blueprint(lab5, url_prefix='/lab5')
 app.register_blueprint(lab6, url_prefix='/lab6')
 app.register_blueprint(lab7, url_prefix='/lab7')
+app.register_blueprint(lab8, url_prefix='/lab8')
 app.register_blueprint(rgz, url_prefix='/rgz') 
 
 # Глобальная переменная для отслеживания состояния ресурса
@@ -41,6 +61,7 @@ def index():
     lab5_url = url_for('lab5.lab')
     lab6_url = url_for('lab6.lab')
     lab7_url = url_for('lab7.lab')
+    lab8_url = url_for('lab8.lab')
     rgz_url = url_for('rgz.books_list') 
 
     return f'''
@@ -63,6 +84,7 @@ def index():
                     <li><a href="{lab5_url}">Лабораторная 5</a></li>
                     <li><a href="{lab6_url}">Лабораторная 6</a></li>
                     <li><a href="{lab7_url}">Лабораторная 7</a></li>
+                    <li><a href="{lab8_url}">Лабораторная 8</a></li>
                     <li><a href="{rgz_url}">RGZ: Книги</a></li>
                 </ul>
             </nav>
