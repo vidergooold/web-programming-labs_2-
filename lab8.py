356b68e106e8d566d736b1e0a99b05c8212cc92c
vidergold <iravidergold@yandex.ru>
2024-12-22 00:03:41 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from werkzeug.security import generate_password_hash
+from db.models import users, articles
+from db import db
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -11,9 +14,31 @@ def lab8_main():
 def login():
     return "Страница входа (в разработке)"
 
-@lab8.route('/register')
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
-    return "Страница регистрации (в разработке)"
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    # Получение данных из формы
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    # Проверки на пустые значения
+    if not login_form or not password_form:
+        return render_template('lab8/register.html', error='Логин и пароль не должны быть пустыми')
+
+    # Проверка существования пользователя с таким логином
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    # Хеширование пароля и добавление нового пользователя
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/lab8/')
 
 @lab8.route('/articles')
 def articles():
